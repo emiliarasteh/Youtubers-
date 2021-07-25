634a1e186911920ccd000ca7416c8b9b01f9925e
emilia <emiliyarasty@yahoo.com>
2021-07-25 22:08:14 +0300

    change edit and delete button

--- resources/views/admin/lessons/index.blade.php
+++ resources/views/admin/lessons/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="x_panel">
         <div class="x_title">
-            <h2>Basic Tables <small>basic table subtitle</small> </h2>
+            <h2>Basic Tables <small>basic table subtitle</small></h2>
             <ul class="nav navbar-right panel_toolbox">
                 <a href="{{route('admin.lessons.create')}}" class="btn btn-round btn-primary">Create</a>
             </ul>
@@ -31,92 +31,107 @@
                         <td>{{$value->duration}}</td>
                         <td>{{$value->video}}</td>
                         <td>{{$value->order}}</td>
+
+                        <!-- modals -->
+                        <!-- Large modal -->
                         <td>
+                            <button type="button" class="btn btn-info btn-sm" data-toggle="modal"
+                                    data-target=".bs-example-modal-lg{{$value->id}}">Edit
+                            </button>
 
-                            <!-- modals -->
-                            <!-- Large modal -->
-                            <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target=".bs-example-modal-lg{{$value->id}}">Edit</button>
-
-                            <div class="modal fade bs-example-modal-lg{{$value->id}}" tabindex="-1" role="dialog" aria-hidden="true">
-                                <div class="modal-dialog modal-lg">
-                                    <div class="modal-content">
-
-                                        <div class="modal-header">
-                                            <h4 class="modal-title" id="myModalLabel">Edit {{$value->title}}</h4>
-                                            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
+                        <a href="{{route('admin.lessons.destroy', $value->id)}}" class="btn btn-round btn-danger btn-sm">Delete</a>
+                        </td>
 
 
-                                            <form class="form-label-left input_mask" method="post" action="{{route('admin.lessons.update', $value->id)}}"
-                                                  enctype="multipart/form-data">
-                                                @method('put')
-                                                @csrf
-                                                <div class="form-group row">
-                                                    <label class="col-form-label col-md-3 col-sm-3 ">Title </label>
-                                                    <div class="col-md-9 col-sm-9 ">
-                                                        <input value="{{$value->title}}" name="title" type="text" class="form-control">
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <label class="col-form-label col-md-3 col-sm-3 ">Description</label>
-                                                    <div class="col-md-9 col-sm-9 ">
-                                                        <input value="{{$value->description}}" name="description" type="text" class="form-control">
-                                                    </div>
-                                                </div>
+                        <div class="modal fade bs-example-modal-lg{{$value->id}}" tabindex="-1" role="dialog"
+                             aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
 
-
-                                                <div class="form-group row">
-                                                    <label class="col-form-label col-md-3 col-sm-3 ">Duration </label>
-                                                    <div class="col-md-9 col-sm-9 ">
-                                                        <input value="{{$value->duration}}" name="duration" type="text" class="form-control number-filter">
-                                                    </div>
-                                                </div>
-
-                                                <div class="form-group row">
-                                                    <label class="control-label col-md-3 col-sm-3 ">Video</label>
-                                                    <div class="col-md-9 col-sm-9 ">
-                                                        <input value="{{$value->video}}" name="video" type="file" class="form-control">
-                                                        <img src="/{{$value->video}}"  alt="Video" class="img-responsive mt-2">
-
-                                                    </div>
-                                                </div>
-
-                                                <div class="form-group row">
-                                                    <label class="col-form-label col-md-3 col-sm-3 ">Order</label>
-                                                    <div class="col-md-9 col-sm-9 ">
-                                                        <input value="{{$value->order}}" name="order" type="text" class="form-control number-filter">
-                                                    </div>
-                                                </div>
-
-                                                <div class="ln_solid"></div>
-                                                <div class="form-group row">
-                                                    <div class="col-md-9 col-sm-9  offset-md-3">
-                                                        <button type="submit" class="btn btn-primary">Update</button>
-                                                        <button type="button" class="btn btn-warning" data-dismiss="modal">Cancel</button>
-                                                    </div>
-                                                </div>
-
-                                            </form>
-                                        </div>
+                                    <div class="modal-header">
+                                        <h4 class="modal-title" id="myModalLabel">Edit {{$value->title}}</h4>
+                                        <button type="button" class="close" data-dismiss="modal"><span
+                                                aria-hidden="true">×</span>
+                                        </button>
                                     </div>
+                                    <div class="modal-body">
 
 
+                                        <form class="form-label-left input_mask" method="post"
+                                              action="{{route('admin.lessons.update', $value->id)}}"
+                                              enctype="multipart/form-data">
+                                            @method('put')
+                                            @csrf
+                                            <div class="form-group row">
+                                                <label class="col-form-label col-md-3 col-sm-3 ">Title </label>
+                                                <div class="col-md-9 col-sm-9 ">
+                                                    <input value="{{$value->title}}" name="title" type="text"
+                                                           class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-form-label col-md-3 col-sm-3 ">Description</label>
+                                                <div class="col-md-9 col-sm-9 ">
+                                                    <input value="{{$value->description}}" name="description"
+                                                           type="text" class="form-control">
+                                                </div>
+                                            </div>
 
+
+                                            <div class="form-group row">
+                                                <label class="col-form-label col-md-3 col-sm-3 ">Duration </label>
+                                                <div class="col-md-9 col-sm-9 ">
+                                                    <input value="{{$value->duration}}" name="duration" type="text"
+                                                           class="form-control number-filter">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label class="control-label col-md-3 col-sm-3 ">Video</label>
+                                                <div class="col-md-9 col-sm-9 ">
+                                                    <input value="{{$value->video}}" name="video" type="file"
+                                                           class="form-control">
+                                                    <img src="/{{$value->video}}" alt="Video"
+                                                         class="img-responsive mt-2">
+
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row">
+                                                <label class="col-form-label col-md-3 col-sm-3 ">Order</label>
+                                                <div class="col-md-9 col-sm-9 ">
+                                                    <input value="{{$value->order}}" name="order" type="text"
+                                                           class="form-control number-filter">
+                                                </div>
+                                            </div>
+
+                                            <div class="ln_solid"></div>
+                                            <div class="form-group row">
+                                                <div class="col-md-9 col-sm-9  offset-md-3">
+                                                    <button type="submit" class="btn btn-primary">Update</button>
+                                                    <button type="button" class="btn btn-warning"
+                                                            data-dismiss="modal">Cancel
+                                                    </button>
+                                                </div>
+                                            </div>
+
+                                        </form>
+                                    </div>
                                 </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
-                                </div>
+
 
                             </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+
+                        </div>
+
         </div>
     </div>
 
 
-    <a href="{{route('admin.lessons.destroy', $value->id)}}" class="btn btn-round btn-primary">Delete</a>
-    </td>
     </tr>
 
     @endforeach
